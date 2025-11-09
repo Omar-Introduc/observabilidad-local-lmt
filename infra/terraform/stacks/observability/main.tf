7e7fb91f28d75ea883eb280a1d2a23d6ce1c7e4c
deryGG <derytv@gmail.com>
2025-11-09 16:22:31 -0500

    estructura de terraform

---
+++ infra/terraform/stacks/observability/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+module "collector"{
+    source = "../modules/collector"
+}
