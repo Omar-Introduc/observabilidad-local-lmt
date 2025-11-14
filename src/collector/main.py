e73db612389a7e6df4b3746806cf6a4fa50a1618
MathiasVilca <randommathias31@gmail.com>
2025-11-14 16:20:06 -0500

    feat(collector): añadido endpoint /ingest/trace

--- src/collector/main.py
+++ src/collector/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.contracts.events import LogEvent, MetricEvent
+from src.contracts.events import LogEvent, MetricEvent, TraceEvent
 import os
 import logging
 import httpx
@@ -77,5 +77,33 @@ def ingest_log(event: LogEvent):
         )
     except Exception as e:
         logger.exception("Error inesperado al reenviar log al store: %s", e)
+
+    return {"status": "ok"}
+
+@app.post("/ingest/trace")
+def ingest_log(event: TraceEvent):
+    """
+    Recibe un TraceEvent, valida automáticamente el esquema usando Pydantic
+    """
+    store_base = os.getenv("STORE_URL", "http://store:8000")
+    store_endpoint = f"{store_base.rstrip('/')}/save/trace"
+
+    logger = logging.getLogger("collector")
+    payload = event.model_dump(mode="json")
+    try:
+        with httpx.Client(timeout=2.0) as client:
+            resp = client.post(store_endpoint, json=payload)
+            resp.raise_for_status()
+            return {"status": "ok", "store_response": resp.json()}
+    except httpx.RequestError as e:
+        logger.warning("No se pudo conectar al Store (%s): %s", store_endpoint, e)
+    except httpx.HTTPStatusError as e:
+        logger.warning(
+            "El Store respondió con error %s: %s",
+            e.response.status_code,
+            e.response.text,
+        )
+    except Exception as e:
+        logger.exception("Error inesperado al reenviar trace al store: %s", e)
 
     return {"status": "ok"}
